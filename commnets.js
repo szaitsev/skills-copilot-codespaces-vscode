fa175492f431dfd2b5a91457d858c45b180f7899
Stanislav Zaitsev <szaitsev@users.noreply.github.com>
2023-11-10 21:20:34 +0000

    Copilot third commit

---
+++ commnets.js
@@ -0,0 +1,31 @@
+//Create web server
+var express = require('express');
+var app = express();
+var fs = require('fs');
+var path = require('path');
+var bodyParser = require('body-parser');
+
+//Create server
+var server = app.listen(8081, function(){
+    var host = server.address().address;
+    var port = server.address().port;
+    console.log('Server running at http://%s:%s',host,port);
+});
+
+//Create application/x-www-form-urlencoded parser
+var urlencodedParser = bodyParser.urlencoded({extended: false});
+
+//Create router
+app.get('/',function(req,res){
+    res.sendFile(__dirname + '/index.html');
+});
+
+app.post('/process_post',urlencodedParser,function(req,res){
+    //Prepare output in JSON format
+    response = {
+        first_name: req.body.first_name,
+        last_name: req.body.last_name
+    };
+    console.log(response);
+    res.end(JSON.stringify(response));
+});
